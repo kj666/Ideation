ae5973f029fc25fe202f8186a659d54a42eab814
Karthikan Jeyabalan <karthi-kan@hotmail.com>
2019-11-09 17:24:50 -0500

    Update package.json dependencies

---
+++ IdeaAPI/Models/Research.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace IdeaAPI.Models
+{
+    public class Research
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string UserName { get; set; }
+
+        public List<string> Keywords { get; set; }
+
+        public List<string> Results { get; set; }
+
+    }
+}
