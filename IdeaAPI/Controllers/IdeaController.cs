e0949ca8448aba28682c2654052f92c995d2f42a
Karthikan Jeyabalan <karthi-kan@hotmail.com>
2019-11-10 00:40:44 -0500

    Connected Wordnik API

--- IdeaAPI/Controllers/IdeaController.cs
+++ IdeaAPI/Controllers/IdeaController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using IdeaAPI.Models;
 using IdeaAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace IdeaAPI.Controllers
 {
@@ -22,5 +26,21 @@ namespace IdeaAPI.Controllers
 
         [HttpGet]
         public ActionResult<List<Research>> Get() => _ideaService.Get();
+
+        [HttpGet("random")]
+        public async Task<List<Word>> CallWordAPI()
+        {
+            using (var client = new HttpClient())
+            {
+                var content = await client.GetStringAsync("https://87938f90-8e2c-4c73-a8c8-b6ca7478ad85.mock.pstmn.io/workAPI");
+                return JsonConvert.DeserializeObject<List<Word>>(content);
+            }
+        }
+
+        public class Word
+        {
+            public int id { get; set; }
+            public string word { get; set; }
+        }
     }
 }
