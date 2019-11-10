1a68005c65346e96279ec96f139247895fcdfd15
Avkash <avkaashsharma89@gmail.com>
2019-11-10 17:02:24 -0500

    Fixed index.html. Added Menu Bar and a Search text field

--- IdeaUI/components/my-element.js
+++ IdeaUI/components/my-element.js
@@ -1,14 +1,35 @@
 import { LitElement, html } from 'lit-element';
+import '@vaadin/vaadin-text-field';
 
-class MyElement extends LitElement{
+class MyElement extends LitElement {
+	static get properties() {
+		return {
+			elements: { type: Array },
+			task: { type: String }
+		};
+	}
 
-    render(){
+	constructor() {
+		super();
+		this.elements = [];
+		this.task = '';
+	}
 
-        return html `
-        <!-- Template Content -->
-        <p> A test </p>
+	render() {
+		return html`
+        <div class="input-layout">
+            <vaadin-text-field
+                placeholder="Search"
+                value="${this.task}"
+                @change="${this.updateTask}"
+            ></vaadin-text-field> 
+        </div>
         `;
-    }
+	}
+
+	updateTask(e) {
+		this.task = e.target.value;
+	}
 }
 
 customElements.define('my-element', MyElement);
