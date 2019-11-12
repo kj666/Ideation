44c0eddd3d13e6223b5207b3275d870230c15fb1
Karthikan Jeyabalan <karthi-kan@hotmail.com>
2019-11-12 06:50:30 -0500

    Fixed bootstrap

--- IdeaUI/components/my-element.js
+++ IdeaUI/components/my-element.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, css, html } from 'lit-element';
 import { HeaderElement } from './header-element';
 import '@vaadin/vaadin-text-field';
 
